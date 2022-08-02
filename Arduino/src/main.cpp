6406d1de46f1144a80f81f50f851c3e4a527c0f6
Gary Allen <gvcallen@gmail.com>
2022-08-02 15:03:56 +0200

    Added code

---
+++ Arduino/src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+#include "hello.h"
+
+void setup() {
+  myFun
+  int x = 5;
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
