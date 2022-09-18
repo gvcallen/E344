e83a277562c37e44886ab91ba324cf18ba32f291
Gary Allen <gvcallen@gmail.com>
2022-09-19 00:42:56 +0200

    A5 working before code update

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,28 +1,84 @@
 #include "Arduino.h"
 
-const int ledPin = 23;
-const int freq = 16;
-const int ledChannel = 0;
-const int resolution = 16;
+#define TRIGGER_PIN 23
+#define CONTROL_PIN 22
+#define BIT_0_PIN 35
+#define BIT_1_PIN 34
+#define BIT_2_PIN 39
+#define BIT_3_PIN 36
+#define ECHO_PIN 14
 
-#define LED 2 // pin 2 will flash for debug only
+const int triggerFreq = 16;
+const int triggerChannel = 0;
+const int triggerRes = 8;
+const int controlFreq = 20000;
+const int controlChannel = 2;
+const int controlRes = 11;
+
+unsigned long echoReceiveTime = 0;
+
+void echoChanged()
+{
+    static unsigned long echoHighTime;
+    
+    uint8_t echoState = digitalRead(ECHO_PIN);
+    if (echoState)
+    {
+        echoHighTime = micros();
+    }
+    else
+    {
+        echoReceiveTime = micros() - echoHighTime;
+    }
+}
+
+void setMotorSpeed(float speedNormalized)
+{
+    speedNormalized *= 0.88f;
+    
+    uint32_t duty = speedNormalized * (1 << controlRes);
+    ledcWrite(controlChannel, duty);
+}
 
 void setup()
 {
-    // put your setup code here, to run once:
-    Serial.begin(115200);                    // uart setup
-    pinMode(LED, OUTPUT);                    // set up pin 2 as output
-    ledcSetup(ledChannel, freq, resolution); // set pwn freq and resulutionf
-    ledcAttachPin(ledPin, ledChannel);       // select pwm outpit pin and channel
-    ledcWrite(ledChannel, 11);               // set dutycycle to 11/65536 (10u out of 62.5ms)
+    Serial.begin(115200);
+
+    // Setup digital input pins
+    pinMode(BIT_0_PIN, INPUT);
+    pinMode(BIT_1_PIN, INPUT);
+    pinMode(BIT_2_PIN, INPUT);
+    pinMode(BIT_3_PIN, INPUT);
+
+    // Setup trigger
+    ledcSetup(triggerChannel, triggerFreq, triggerRes);
+    ledcAttachPin(TRIGGER_PIN, triggerChannel);
+    ledcWrite(triggerChannel, 11);               // set dutycycle to 11/65536 (10u out of 62.5ms)
+
+    // Setup control    
+    ledcSetup(controlChannel, controlFreq, controlRes);
+    ledcAttachPin(CONTROL_PIN, controlChannel);
+    ledcWrite(controlChannel, 8);
+
+    // Setup echo
+    pinMode(ECHO_PIN, INPUT);
+    attachInterrupt(digitalPinToInterrupt(ECHO_PIN), echoChanged, CHANGE);
 }
 
 void loop()
 {
-    // This is just for debug. LED flashes and write to uart.
-    delay(500);
-    digitalWrite(LED, HIGH);
-    delay(500);
-    digitalWrite(LED, LOW);
-    Serial.println("Heart-beat");
+    // Read in 4-bit digital value and store as 4-bit number in speedCommand
+    uint8_t bit0 = digitalRead(BIT_0_PIN);
+    uint8_t bit1 = digitalRead(BIT_1_PIN);
+    uint8_t bit2 = digitalRead(BIT_2_PIN);
+    uint8_t bit3 = digitalRead(BIT_3_PIN);
+    uint8_t speedCommand = (bit0) | (bit1 << 1) | (bit2 << 2) | (bit3 << 3);
+    
+    float distance = (343.0f * echoReceiveTime * 1e-6) / 2;
+    
+    if (distance > 1) distance = 1;
+    
+    float controlCommand = ((float)speedCommand / 15.0f) * distance;
+    setMotorSpeed(controlCommand);
+
 }
